1084932e0bf9787f9b3f73c2d6ba72629813f849
peteandree <118107657+peteandree@users.noreply.github.com>
2022-12-05 07:33:56 +0800

    Update FEXER04.c

--- FEXER04.c
+++ FEXER04.c
@@ -1,27 +1,21 @@
 #include <stdio.h>
+
 int main()
 {
-	int num1;
-	int num2;
-	int i;
+	const float I=0.1;
+	int amt;
+	int yrs;
+	float total;
 	
-	num1 = 0;
-	num2 = 0;
-	i = 0;
+	printf("Input Amount:");
+	scanf("%d",&amt);
+	printf("Input Years:");
+	scanf("%d",&yrs);
 	
-	printf("Enter first number:");
-	scanf("%d", &num1);
-	printf("Enter second number:");
-	scanf("%d", &num2);
+	total=amt*I*yrs+amt;
+
 	
-	while(num1>=num2)
-	{
-		num1=num1-num2;
-		i++;
-	}
-	
-	printf("Sum: %d",i);
-	printf("\nRemainder: %d",num1);
+	printf("\nThe Total Is:%f",total);
 	
 	return 0;
 }
